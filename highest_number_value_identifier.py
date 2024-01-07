8764704942717cacb25d9c19c813d57752050df0
unknown <miggy.payumo26@gmail.com>
2024-01-07 17:43:14 +0800

    test

---
+++ highest_number_value_identifier.py
@@ -0,0 +1,9 @@
+# TASK:
+# Ask user to input 3 numbers, find and print the biggest number using only if statements.
+
+# Pseudocode
+# Ask users to input at least 3 numbers.
+# Evaluate the numbers using mainly "if" statements.
+# Print the number that is highest in its value.
+
+# MAIN CODE
