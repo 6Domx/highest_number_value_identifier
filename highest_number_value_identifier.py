67e9aedb146a80d4cf50b07db67ba9c4ab3c9f81
unknown <miggy.payumo26@gmail.com>
2024-01-07 18:00:37 +0800

    window config done

--- highest_number_value_identifier.py
+++ highest_number_value_identifier.py
@@ -7,3 +7,23 @@
 # Print the number that is highest in its value.
 
 # MAIN CODE
+# 1. Imports
+
+import sqlite3
+from tkinter import *
+from tkinter import Tk
+from tkinter import messagebox
+from PIL import ImageTk, Image
+
+# 2. Window Configuration
+
+root = Tk()
+root.title("Highest Number Value Identifier")
+root.geometry("600x480")
+image_0=Image.open("C:\\Users\\M2\\Documents\\CODES\\Projects\\PYTHON\\highest_number_value_identifier\\image.png")
+bck_end=ImageTk.PhotoImage(image_0)
+lbl=Label(root,image=bck_end)
+lbl.place(x=0,y=0)
+root.resizable(False,False)
+
+root.mainloop()
